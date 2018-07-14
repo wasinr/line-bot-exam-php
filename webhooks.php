dc6b723fda3a989d97be5765b55f6db8f98efc8c
Wasin Raktham <wasin.r@halalize.com>
2018-07-14 15:38:24 +0700

    add share location

--- webhooks.php
+++ webhooks.php
@@ -13,57 +13,66 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 
 
-	// Loop through each event
-	foreach ($events['events'] as $event) {
+    // Loop through each event
+    foreach ($events['events'] as $event) {
 
 
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
 
-
-		    if($event['message']['text']=='เวลาละหมาด'){
+            if ($event['message']['text'] == 'เวลาละหมาด') {
                 $text = 'เวลาละหมาดของวันที่ 14 กรกฎาคม 2561 คือ...';
             }
-            if($event['message']['text']=='ร้านอาหารฮาลาล'){
+            if ($event['message']['text'] == 'ร้านอาหารฮาลาล') {
                 $text = 'ร้านอาหารฮาลาลใกล้เคียงคือ';
             }
-            if($event['message']['text']=='ช้อปปิ้ง'){
+            if ($event['message']['text'] == 'ช้อปปิ้ง') {
                 $text = 'ต้องการช้อปปิ้งอะไรดีครับ';
             }
 
-			// Get text sent
-			//$text = $event['source']['userId'];
+            // Get text sent
+            //$text = $event['source']['userId'];
 
             // Get replyToken
-			$replyToken = $event['replyToken'];
+            $replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+            // Build message to reply back
+            /*
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
+            */
+            $messages = [
+                'type' => 'buttons',
+                'text' => 'แชร์โลเคชั่น',
+                'actions' => [array(
+                                "type"=> "postback",
+                                "label"=> "Buy",
+                                "data"=> "action=buy&itemid=123"
+                                )]
+                ];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [ $messages ],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
 
-			echo $result . "\r\n";
-		}
-	}
+            echo $result . "\r\n";
+        }
+    }
 }
-echo "OK";
